0160b347b79e92ed429e8db87795f5f2b72b2959
Renanfls <contato.renanfabricio@gmail.com>
2024-08-05 23:41:01 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
+
+db = SQLAlchemy(app)
+
+# Molde do Produto
+class Product(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(120), nullable=False)
+  price = db.Column(db.Float, nullable=False)
+  description = db.Column(db.Text, nullable=True)
+
+# === ROUTES ===
+# ADD
+@app.route('/api/products/add', methods=["POST"])
+def add_product():
+  data = request.json
+  if 'name' in data and 'price' in data:
+    product = Product(name=data['name'], price=data['price'], description=data.get('description' , ""))
+    db.session.add(product)
+    db.session.commit()
+    return jsonify({"message": "Product added successfully"})
+  return jsonify({"message": "Invalid product data"}), 400
+
+# DELETE
+@app.route('/api/products/delete/<int:product_id>', methods=["DELETE"])
+def delete_product(product_id):
+  product = Product.query.get(product_id)
+  if product:
+    db.session.delete(product)
+    db.session.commit()
+    return jsonify({"message": "Product deleted successfully"})
+  return jsonify({"message": "Product not found"}), 404
+
+# /
+@app.route('/')
+def home():
+  return 'Hello World'
+  
+if __name__ == "__main__":
+	app.run(debug=True)
